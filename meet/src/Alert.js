8e2284a5bdbba79860cb5ff6e2ee253f123074a5
edwinmoraa <58499972+edwinmoraa@users.noreply.github.com>
2022-07-31 00:01:04 -0400

    google verification

--- meet/src/Alert.js
+++ meet/src/Alert.js
@@ -40,4 +40,11 @@ class ErrorAlert extends Alert {
     }
   }
 
-export { InfoAlert, ErrorAlert };
+  class OfflineAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = 'red';
+  }
+}
+
+export { InfoAlert, ErrorAlert, OfflineAlert };
