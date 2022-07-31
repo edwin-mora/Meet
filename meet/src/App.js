59770e69dc0705fa32ca54ed1d757c4bb0e02dc8
edwinmoraa <58499972+edwinmoraa@users.noreply.github.com>
2022-07-31 00:23:46 -0400

    welcome

--- meet/src/App.js
+++ meet/src/App.js
@@ -1,60 +1,84 @@
-import React, { Component } from 'react';
-import './App.css';
-import EventList from './EventList';
-import CitySearch from './CitySearch';
-import NumberOfEvents from './NumberOfEvents';
-import { extractLocations, getEvents } from './api';
-import './nprogress.css/nprogress.css';
+import React, { Component } from "react";
+import "./App.css";
+import EventList from "./EventList";
+import CitySearch from "./CitySearch";
+import NumberOfEvents from "./NumberOfEvents";
+import { extractLocations, getEvents, checkToken, getAccessToken } from "./api";
+import "./nprogress.css/nprogress.css";
+import WelcomeScreen from "./WelcomeScreen";
 
 class App extends Component {
   state = {
     events: [],
     locations: [],
-    numberOfEvents: 32
+    numberOfEvents: 2,
+    showWelcomeScreen: undefined,
+  };
+  // loading events when the app loads
+
+  async componentDidMount() {
+    this.mounted = true;
+    const accessToken = localStorage.getItem("access_token");
+    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get("code");
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    if ((code || isTokenValid) && this.mounted) {
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({ events, locations: extractLocations(events) });
+        }
+      });
+    } 
   }
 
+  //offline
+
+
   render() {
+    if (this.state.showWelcomeScreen === undefined)
+      return <div className="App" />;
     return (
       <div className="App">
-        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
+        <CitySearch
+          locations={this.state.locations}
+          updateEvents={this.updateEvents}
+        />
         <EventList events={this.state.events} />
-        <NumberOfEvents updateEvents={this.updateEvents}  />
-
+        <NumberOfEvents updateEvents={this.updateEvents} />
+        <WelcomeScreen
+          showWelcomeScreen={this.state.showWelcomeScreen}
+          getAccessToken={() => {
+            getAccessToken();
+          }}
+        />
       </div>
     );
   }
   //update events method
   updateEvents = (location, maxEvents) => {
-    if (maxEvents === undefined){
+    if (maxEvents === undefined) {
       maxEvents = this.state.numberOfEvents;
     } else {
-      this.setState({ numberOfEvents: maxEvents })
+      this.setState({ numberOfEvents: maxEvents });
     }
     if (location === undefined) {
-      location = this.state.locationEvents
+      location = this.state.locationEvents;
     }
     getEvents().then((events) => {
-      const locationEvents = (location === 'all') ?
-        events :
-        events.filter((event) => event.location === location);
+      const locationEvents =
+        location === "all"
+          ? events
+          : events.filter((event) => event.location === location);
       this.setState({
-        events: locationEvents
+        events: locationEvents,
       });
     });
-  }
-
-  // loading events when the app loads
-  componentDidMount() {
-    getEvents().then((events) => {
-      this.setState({ events, locations: extractLocations(events) });
-    });
-  }
+  };
 
   componentWillUnmount() {
     this.mounted = false;
   }
-
-
 }
 
 export default App;
