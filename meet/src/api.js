cde3c0c03392adfeeb132fe285226bfdc6a1c735
edwinmoraa <58499972+edwinmoraa@users.noreply.github.com>
2022-07-30 22:58:55 -0400

    appshell

--- meet/src/api.js
+++ meet/src/api.js
@@ -43,6 +43,7 @@ export const getEvents = async () => {
   if (!navigator.onLine) {
     const data = localStorage.getItem("lastEvents");
     NProgress.done();
+    // eslint-disable-next-line no-undef
     return data ? JSON.parse(events).events : [];;
   }
 
